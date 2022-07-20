538766e107a8c09b16dc87dd01a7fcb098656ee7
Ait0ne666 <yukhanov@lilekov-studio.com>
2022-07-20 19:59:10 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
 
-
+  async redirects() {
+    return [
+      {
+        source: '/api/:slug',
+        destination: 'http://82.202.165.88:8085/:slug', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
