5bfc18c2c700fbdc28f39f2ca606a03f343a8e8a
Ait0ne666 <yukhanov@lilekov-studio.com>
2022-07-25 11:55:06 +0300

    fix

--- src/components/Organisms/Registration/registration.tsx
+++ src/components/Organisms/Registration/registration.tsx
@@ -1,4 +1,5 @@
 import { toast } from "@chakra-ui/react"
+import { useRouter } from "next/router"
 import { useState, useEffect, ChangeEventHandler } from "react"
 import { auth, getJwt, getUser, register, ymAuth } from "../../../data/api/api"
 import { useCustomToast } from "../../../hooks/toast"
@@ -8,6 +9,7 @@ const RegistrationModal = () => {
 
     const [registered, setRegistered] = useState(false)
     const toast = useCustomToast()
+    const router = useRouter()
 
     useEffect(() => {
 
@@ -17,31 +19,38 @@ const RegistrationModal = () => {
 
         if (user && jwt) {
             auth(jwt)
-            .catch((err) => {
-                console.log(err)
-            })
+                .catch((err) => {
+                    console.log(err)
+                })
             return
-        }
+        } else {
+            let query = window.location.search
 
 
-        let query = window.location.search
+            if (query.includes("jwt")) {
+                query = query.replace("?", "")
+                const parts = query.split("=")
+                if (parts.length > 1) {
+                    const jwt = parts[1]
 
-
-        if (query.includes("jwt")) {
-            query = query.replace("?", "")
-            const parts = query.split("=")
-            if (parts.length > 1) {
-                const jwt = parts[1]
-                
-                auth(jwt)
-                .then(() => {
-                    localStorage.setItem("belster-jwt", jwt)
-                })
-                .catch((err ) => {
-                    toast("", "error", err.message)
-                })
+                    auth(jwt)
+                        .then(() => {
+                            localStorage.setItem("belster-jwt", jwt)
+                        })
+                        .catch((err) => {
+                            toast("", "error", err.message)
+                            router.push("/404")
+                        })
+                } else {
+                    router.push("/404")
+                }
+            } else {
+                router.push("/404")
             }
+
         }
+
+
 
 
     }, [])
