9ef7ada070f9d98fa93ec93294066dc8f1055c5a
Ait0ne666 <yukhanov@lilekov-studio.com>
2022-08-15 07:50:25 +0300

    fixed file upload

--- src/components/Atoms/FileUpload/file-upload.tsx
+++ src/components/Atoms/FileUpload/file-upload.tsx
@@ -38,6 +38,7 @@ const FileUpload: React.FC<FileUploadParams> = ({
                 return;
             }
             onUpload(file)
+            uploadRef.current!.value = ""
         }
     };
 
